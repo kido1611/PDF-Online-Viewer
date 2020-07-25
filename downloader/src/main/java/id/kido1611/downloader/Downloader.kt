6fd9e3ab45cadc0e85daa5ce17bedc5fabcbf7e4
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2020-07-25 20:09:19 +0700

    Change KTOR downloader with HttpURLConnection

--- downloader/src/main/java/id/kido1611/downloader/Downloader.kt
+++ downloader/src/main/java/id/kido1611/downloader/Downloader.kt
@@ -1,84 +1,116 @@
 package id.kido1611.downloader
 
 import android.content.Context
-import io.ktor.client.HttpClient
-import io.ktor.client.call.call
-import io.ktor.client.engine.android.Android
-import io.ktor.client.request.url
-import io.ktor.http.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.withContext
+import java.io.BufferedInputStream
 import java.io.File
-import kotlin.math.roundToInt
+import java.io.FileOutputStream
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 object Downloader {
-    private val httpClient = HttpClient(Android)
+    private const val BUFFER_LEN = 1 * 1024
+    private const val NOTIFY_PERIOD = 150 * 1024
 
-    fun downloadFile(context: Context, url: String) : Flow<DownloadResult>{
-        return downloadFile(context, url, null)
+    private var isRunning = false
+
+    fun downloadFile(
+        context: Context,
+        url: String,
+        path: File = context.filesDir
+    ): Flow<DownloadResult> {
+        return downloadFile(context, url, null, path)
     }
 
-    fun downloadFile(context: Context, url: String, fileName: String) : Flow<DownloadResult>{
-        val file = File(context.filesDir, fileName)
-        return downloadFile(context, url, file)
+    fun downloadFile(
+        context: Context,
+        url: String,
+        fileName: String,
+        path: File = context.filesDir
+    ): Flow<DownloadResult> {
+        val file = File(path, fileName)
+        return downloadFile(context, url, file, path)
     }
 
-    fun downloadFile(context: Context, url: String, file: File?) : Flow<DownloadResult>{
+    private fun downloadFile(
+        context: Context,
+        url: String,
+        file: File?,
+        path: File = context.filesDir
+    ): Flow<DownloadResult> {
+        isRunning = true
         return flow {
-            if(file != null && file.exists() && file.length() > 0L){
+            if (file != null && file.exists() && file.length() > 0L) {
                 emit(DownloadResult.Success(file))
-            }
-            else{
+            } else {
                 try {
-                    val response = httpClient.call {
-                        url(url)
-                        method = HttpMethod.Get
-                    }.response
-
-                    val data = ByteArray(response.contentLength()!!.toInt())
-                    var offset = 0
-
-                    val contentDisposition = ContentDisposition.parse(response.headers[HttpHeaders.ContentDisposition].toString())
 
                     var savedFile = file
-                    if(savedFile == null){
-                        savedFile = File(context.filesDir, contentDisposition.parameter(ContentDisposition.Parameters.FileName) ?: System.currentTimeMillis().toString())
-                    }
 
-                    if(savedFile.exists() && savedFile.length() > 0){
-                        emit(DownloadResult.Success(savedFile))
-                        httpClient.close()
-                    }
-                    else{
-                        do{
-                            val currentRead = response.content.readAvailable(data, offset, data.size)
-                            offset += currentRead
-                            val progress = (offset * 100f / data.size).roundToInt()
-                            emit(DownloadResult.Progress(progress))
-                        } while (currentRead > 0)
-                        response.close()
+                    val urlObject = URL(url)
+                    val urlConnection = urlObject.openConnection() as HttpURLConnection
+                    val totalSize = urlConnection.contentLength
 
-                        if(response.status.isSuccess()){
-                            withContext(Dispatchers.IO){
-                                savedFile.outputStream().write(data)
+                    if (savedFile == null) {
+                        val contentDisposition = urlConnection.getHeaderField("Content-Disposition")
+
+                        savedFile = File(
+                            path,
+                            if (contentDisposition.isNullOrBlank()) {
+                                System.currentTimeMillis().toString()
+                            } else {
+                                contentDisposition.split("=")[1]
                             }
+                        )
+                    }
+
+                    if (savedFile.length() > 0) {
+                        emit(DownloadResult.Success(savedFile))
+                        urlConnection.disconnect()
+                    } else {
+                        var downloadedSize = 0
+                        var counter = 0
+                        val buffer = ByteArray(BUFFER_LEN)
+
+                        val downloadStream = BufferedInputStream(urlConnection.inputStream)
+
+                        var bufferLength = 0
+                        val outputStream = FileOutputStream(savedFile)
+
+                        while (downloadStream.read(buffer)
+                                .also { bufferLength = it } > 0 && isRunning
+                        ) {
+                            outputStream.write(buffer, 0, bufferLength)
+                            downloadedSize += bufferLength
+                            counter += bufferLength
+
+                            if (counter > NOTIFY_PERIOD) {
+                                val progress = downloadedSize * 100 / totalSize
+                                emit(DownloadResult.Progress(progress))
+                                counter = 0
+                            }
+                        }
+
+                        urlConnection.disconnect()
+                        outputStream.close()
+
+                        if (isRunning) {
                             emit(DownloadResult.Success(savedFile))
                         }
-                        else{
-                            emit(DownloadResult.Error("File not downloaded"))
-                        }
                     }
-                }
-                catch (e: TimeoutCancellationException){
-                    emit(DownloadResult.Error("Connection timeout", e))
-                }
-                catch (t: Throwable){
-                    emit(DownloadResult.Error("Failed to connect, ${t.message}"))
+                } catch (e: MalformedURLException) {
+                    emit(DownloadResult.Error("Url not valid", e))
+                } catch (e: IOException) {
+                    emit(DownloadResult.Error("Failed", e))
                 }
             }
         }
+    }
+
+    fun cancel() {
+        isRunning = false
     }
 }
